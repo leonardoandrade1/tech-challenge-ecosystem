f54847da0b457d0ef8f4720fe0f1af23eb562ac1
Leonardo de Andrade <leonardoandrade0@outlook.com>
2024-09-09 18:49:30 -0300

    feat: add transactions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, TransactionsModule],
 })
 export class AppModule {}
