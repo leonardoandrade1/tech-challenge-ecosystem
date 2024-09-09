d689e816e366476eb0df1276c1873eb0a2f258b5
Leonardo de Andrade <leonardoandrade0@outlook.com>
2024-09-09 20:45:27 -0300

    feat: add MerchantPayable model and typeorOrm configuration into PayableModule

--- src/modules/payables/payables.module.ts
+++ src/modules/payables/payables.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PayablesController } from './payables.controller';
+import { MerchantPayable } from 'src/shared/domain/models';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MerchantPayable])],
   controllers: [PayablesController],
   providers: [],
 })
