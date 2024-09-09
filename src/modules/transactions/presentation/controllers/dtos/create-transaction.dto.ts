61ab47e04e84e8f3942ef4616abf13ead980c123
Leonardo de Andrade <leonardoandrade0@outlook.com>
2024-09-09 20:21:15 -0300

    refactor: move CreateTransactionDto and add class validator

--- src/modules/transactions/presentation/controllers/dtos/create-transaction.dto.ts
+++ src/modules/transactions/presentation/controllers/dtos/create-transaction.dto.ts
@@ -1,12 +1,51 @@
+import {
+  IsCreditCard,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { PaymentMethod } from 'src/shared/domain/enums';
 
 export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
   merchantId: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsEnum(PaymentMethod)
+  @IsNotEmpty()
   paymentMethod: PaymentMethod;
+
+  @IsNumber()
+  @IsPositive()
   amount: number;
+
+  @IsCreditCard()
+  @IsNotEmpty()
   cardNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   cardHolder: string;
+
+  @IsNotEmpty()
+  @Matches(/^(0[1-9]|1[0-2])\/?(20[0-9]{2})$/, {
+    message: 'Card expiration date must follow the pattern: DD/YYYY',
+  })
   cardExpirationDate: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(3)
   cvv: string;
 }
