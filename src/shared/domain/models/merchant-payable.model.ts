19b604f6b257d741dd0aee36f8f562e016f5f58d
Leonardo de Andrade <leonardoandrade0@outlook.com>
2024-09-09 20:39:11 -0300

    feat: add payable calcutation from transaction values and payment method

--- src/shared/domain/models/merchant-payable.model.ts
+++ src/shared/domain/models/merchant-payable.model.ts
@@ -1,15 +1,19 @@
+import { BadRequestException, NotAcceptableException } from '@nestjs/common';
 import {
   Column,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { PayableStatus, PaymentMethod } from '../enums';
 import { ColumnNumericTransformer } from '../utils/column-numeric-transformer.typeorm';
-import { PayableStatus } from '../enums';
-import { BadRequestException } from '@nestjs/common';
 
 @Entity('Payable')
 export class MerchantPayable {
+  static DEBIT_FEE = 0.02;
+  static CREDIT_FEE = 0.04;
+  static DAYS_TO_ADD_TO_CREDIT = 30;
+
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -49,5 +53,50 @@ export class MerchantPayable {
     model.merchantId = merchantId;
     model.transactionId = transactionId;
     return model;
+  }
+
+  calculateTransaction(
+    paymentMethod: PaymentMethod,
+    amount: number,
+    transactionDate: Date,
+  ): void {
+    if (amount <= 0)
+      throw new BadRequestException(
+        'only positive values are accepted to calculate payable',
+      );
+    switch (paymentMethod) {
+      case PaymentMethod.DebitCard:
+        this.calculateDebit(amount, transactionDate);
+        break;
+      case PaymentMethod.CreditCard:
+        this.calculateCredit(amount, transactionDate);
+        break;
+
+      default:
+        throw new NotAcceptableException('payment method not acceptable');
+    }
+  }
+
+  private calculateDebit(amount: number, transactionDate: Date): void {
+    const discount = amount * MerchantPayable.DEBIT_FEE;
+    const total = amount - discount;
+    this.status = PayableStatus.Paid;
+    this.createdAt = transactionDate;
+    this.subTotal = amount;
+    this.discount = discount;
+    this.total = total;
+  }
+
+  private calculateCredit(amount: number, transactionDate: Date): void {
+    transactionDate.setDate(
+      transactionDate.getDate() + MerchantPayable.DAYS_TO_ADD_TO_CREDIT,
+    );
+    const discount = amount * MerchantPayable.CREDIT_FEE;
+    const total = amount - discount;
+    this.status = PayableStatus.WaitingFunds;
+    this.createdAt = transactionDate;
+    this.subTotal = amount;
+    this.discount = discount;
+    this.total = total;
   }
 }
--- src/shared/domain/models/tests/merchant-payable.model.spec.ts
+++ src/shared/domain/models/tests/merchant-payable.model.spec.ts
@@ -1,4 +1,5 @@
 import { BadRequestException } from '@nestjs/common';
+import { PaymentMethod } from '../../enums';
 import { MerchantPayable } from '../merchant-payable.model';
 
 describe('MerchantPayable', () => {
@@ -42,6 +43,103 @@ describe('MerchantPayable', () => {
           params.transactionId,
         ),
       ).toBeInstanceOf(MerchantPayable);
+    });
+  });
+
+  describe('calculateTransaction', () => {
+    const fakeDate = new Date('2024-01-01T00:00:00.000Z');
+    beforeAll(() => {
+      jest
+        .useFakeTimers({ doNotFake: ['performance'] })
+        .setSystemTime(fakeDate);
+    });
+
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    it('should throw BadRequestException when trying to calculate transaction with negative or zero amount', () => {
+      const params = {
+        merchantId: '123',
+        transactionId: 1,
+        transactionDate: fakeDate,
+        paymentMethod: PaymentMethod.DebitCard,
+      };
+
+      const payable = MerchantPayable.NewFromTransaction(
+        params.merchantId,
+        params.transactionId,
+      );
+      expect(() =>
+        payable.calculateTransaction(
+          params.paymentMethod,
+          -1,
+          params.transactionDate,
+        ),
+      ).toThrow(BadRequestException);
+      expect(() =>
+        payable.calculateTransaction(
+          params.paymentMethod,
+          0,
+          params.transactionDate,
+        ),
+      ).toThrow(BadRequestException);
+    });
+
+    it('should calculate debit no adding days and discoutning correct fees', () => {
+      const params = {
+        merchantId: '123',
+        transactionId: 1,
+        amount: 100,
+        transactionDate: fakeDate,
+        paymentMethod: PaymentMethod.DebitCard,
+      };
+
+      const expectedDiscount = params.amount * MerchantPayable.DEBIT_FEE;
+      const expectedTotal = params.amount - expectedDiscount;
+      const payable = MerchantPayable.NewFromTransaction(
+        params.merchantId,
+        params.transactionId,
+      );
+      payable.calculateTransaction(
+        params.paymentMethod,
+        params.amount,
+        params.transactionDate,
+      );
+      expect(payable.createdAt).toEqual(params.transactionDate);
+      expect(payable.subTotal).toEqual(params.amount);
+      expect(payable.discount).toEqual(expectedDiscount);
+      expect(payable.total).toEqual(expectedTotal);
+    });
+
+    it('should calculate credit adding credit payable days and discoutning correct fees', () => {
+      const params = {
+        merchantId: '123',
+        transactionId: 1,
+        amount: 100,
+        transactionDate: fakeDate,
+        paymentMethod: PaymentMethod.CreditCard,
+      };
+
+      const expectedDiscount = params.amount * MerchantPayable.CREDIT_FEE;
+      const expectedTotal = params.amount - expectedDiscount;
+      const expectedPayableDate = new Date();
+      expectedPayableDate.setDate(
+        expectedPayableDate.getDate() + MerchantPayable.DAYS_TO_ADD_TO_CREDIT,
+      );
+      const payable = MerchantPayable.NewFromTransaction(
+        params.merchantId,
+        params.transactionId,
+      );
+      payable.calculateTransaction(
+        params.paymentMethod,
+        params.amount,
+        params.transactionDate,
+      );
+      expect(payable.createdAt).toEqual(expectedPayableDate);
+      expect(payable.subTotal).toEqual(params.amount);
+      expect(payable.discount).toEqual(expectedDiscount);
+      expect(payable.total).toEqual(expectedTotal);
     });
   });
 });
