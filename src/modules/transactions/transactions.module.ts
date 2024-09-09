11f77ea00044e52a2ad5167311225abd8f359344
Leonardo de Andrade <leonardoandrade0@outlook.com>
2024-09-09 18:51:05 -0300

    feat: add configuration for merchantTransaction model into transaction typeorm module

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MerchantTransaction } from 'src/shared/domain/models';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MerchantTransaction])],
   controllers: [TransactionsController],
   providers: [],
 })
